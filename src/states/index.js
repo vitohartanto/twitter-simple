1248ee123b67e52e70fddb7a0742211266d73a14
Vito <vitohartanto@mail.ugm.ac.id>
2024-04-28 22:22:17 +0700

    configure store and making states folder

---
+++ src/states/index.js
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { loadingBarReducer } from 'react-redux-loading-bar';
+import authUserReducer from './authUser/reducer';
+import isPreloadReducer from './isPreload/reducer';
+import leaderboardsReducer from './leaderboards/reducer';
+import threadsReducer from './threads/reducer';
+import usersReducer from './users/reducer';
+import threadDetailReducer from './threadDetail/reducer';
+import { thunk } from 'redux-thunk';
+
+const store = configureStore({
+  reducer: {
+    authUser: authUserReducer,
+    isPreload: isPreloadReducer,
+    users: usersReducer,
+    threads: threadsReducer,
+    threadDetail: threadDetailReducer,
+    leaderboards: leaderboardsReducer,
+    loadingBar: loadingBarReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
+
+export default store;
