a159276003eaae57c10e85af9cca611e6921fdb8
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-02 08:25:59 +0700

    not found page

--- src/pages/NotFound404Page.jsx
+++ src/pages/NotFound404Page.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import landscapeBackground from '../img/nicePageLeaderboards.png';
 import ImageBackground from '../components/ImageBackground';
 import NavigationBar from '../components/NavigationBar';
-import { Link } from 'react-router-dom';
 
-const NotFound404Page = () => {
+function NotFound404Page() {
   return (
     <div>
       <ImageBackground
@@ -11,13 +12,18 @@ const NotFound404Page = () => {
         hash="[HD1_gMJ01yWL0RRM1$}NXIcaR-9_,noFrk.5PxGrdNxVycBx?s:xvRPr?xCJ*ahkEWA+ia{XNob"
       />
       <NavigationBar />
-      <div className="p-8 gap-8 flex flex-col items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[18px] bg-[rgba(25,25,25,0.90)]">
+      <div className="p-8 gap-8 flex flex-col items-center
+      fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+      backdrop-blur-[2px] border-[1px_solid_rgba(255,255,255,0.18)]
+      shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[18px] bg-[rgba(25,25,25,0.90)]"
+      >
         <h1 className="text-5xl font-bold text-white">404</h1>
         <h1 className="text-5xl font-bold text-white text-center">
           PAGE NOT FOUND
         </h1>
         <h1 className="text-3xl font-semibold text-white text-center">
-          Back to{' '}
+          Back to
+          {' '}
           <Link to="/" className="text-3xl font-semibold text-blue-500">
             Home
           </Link>
@@ -25,6 +31,6 @@ const NotFound404Page = () => {
       </div>
     </div>
   );
-};
+}
 
 export default NotFound404Page;
