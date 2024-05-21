b1afe180d50e5278045239dc6b1c03c55f827e37
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-21 12:57:49 +0700

    threads testing 1 done

---
+++ src/states/threads/reducer.test.js
@@ -0,0 +1,36 @@
+import { describe, it, expect } from 'vitest';
+import threadsReducer from './reducer';
+
+const THREADS_ACTION_TYPES = {
+  SEE_THREADS: 'threads/SEE_THREADS',
+  CREATE_THREAD: 'threads/CREATE_THREAD',
+  UP_VOTE_THREAD: 'threads/UP_VOTE_THREAD',
+  NEUTRALIZE_VOTE_THREAD: 'threads/NEUTRALIZE_VOTE_THREAD',
+  DOWN_VOTE_THREAD: 'threads/DOWN_VOTE_THREAD',
+};
+
+/**
+ * test scenario for threadsReducer
+ *
+ * - threadsReducer function
+ *  - should return the initial state when given by unknown action
+ *  - should return threads when given by threads/SEE_THREADS action
+ *  - should return the threads with the new thread when given by threads/CREATE_THREAD action
+ *  - should return the threads with the up voted thread when given by threads/UP_VOTE_THREAD action
+ *  - should return the threads with the neutralize voted thread when given by threads/NEUTRALIZE_VOTE_THREAD action
+ *  - should return the threads with the down voted thread when given by threads/DOWN_VOTE_THREAD action
+ */
+
+describe('threadsReducer function', () => {
+  it('should return the initial state when given by unknown action', () => {
+    // Arrange
+    const initialState = [];
+    const action = { type: 'UNKNOWN' };
+
+    // Action
+    const nextState = threadsReducer(initialState, action);
+
+    // Assert
+    expect(nextState).toEqual(initialState);
+  });
+});
