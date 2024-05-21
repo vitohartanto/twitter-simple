fb533d8c83fe67e6d88123bcf1b5cb151e4e0aba
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-21 14:57:59 +0700

    users state reducer testing done

--- src/states/users/reducer.test.js
+++ src/states/users/reducer.test.js
@@ -9,7 +9,8 @@ const USERS_ACTION_TYPES = {
  * test scenario for usersReducer
  *
  * - usersReducer function
- *  - should return the initial state when given by unknow action
+ * - should return users when given by users/SEE_USERS action
+ *  - should return the initial state when given by unknown action
  */
 
 describe('usersReducer function', () => {
@@ -23,5 +24,27 @@ describe('usersReducer function', () => {
 
     // Assert
     expect(nextState).toEqual(initialState);
+  });
+
+  it('should return users when given by users/SEE_USERS action', () => {
+    // Arrange
+    const initialState = [];
+    const action = {
+      type: USERS_ACTION_TYPES.SEE_USERS,
+      payload: {
+        users: {
+          id: 'john_doe',
+          name: 'John Doe',
+          email: 'john@example.com',
+          avatar: 'https://ui-avatars.com/api/?name=John Doe&background=random',
+        },
+      },
+    };
+
+    // Action
+    const nextState = usersReducer(initialState, action);
+
+    // Assert
+    expect(nextState).toEqual(action.payload.users);
   });
 });
