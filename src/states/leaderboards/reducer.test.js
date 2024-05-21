cdefee610ace0dcd655881cf0f3829ab18852939
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-21 11:33:51 +0700

    leaderboards testing done

--- src/states/leaderboards/reducer.test.js
+++ src/states/leaderboards/reducer.test.js
@@ -10,7 +10,7 @@ const LEADERBOARDS_ACTION_TYPES = {
  *
  * - leaderboardsReducer function
  *  - should return the initial state when given by unknown action
- *  - should return the isPreload with value boolen when given by isPreload/SET_IS_PRELOAD action
+ *  - should return the leaderboards when given by leaderboards/SEE_LEADERBOARDS action
  */
 
 describe('leaderboardsReducer function', () => {
@@ -26,5 +26,53 @@ describe('leaderboardsReducer function', () => {
 
     // Assert
     expect(nextState).toEqual(initialState);
+  });
+
+  it('should return the leaderboards when given by leaderboards/SEE_LEADERBOARDS action', () => {
+    // Arrange
+    const initialState = [];
+    const action = {
+      type: LEADERBOARDS_ACTION_TYPES.SEE_LEADERBOARDS,
+      payload: {
+        leaderboards: [
+          {
+            user: {
+              id: 'user-dimassaputra',
+              name: 'Dimas Saputra',
+              email: 'dimas@dicoding.com',
+              avatar:
+                'https://ui-avatars.com/api/?name=Dimas Saputra&background=random',
+            },
+            score: 25,
+          },
+          {
+            user: {
+              id: 'user-dicoding',
+              name: 'Dicoding',
+              email: 'admin@dicoding.com',
+              avatar:
+                'https://ui-avatars.com/api/?name=Dicoding&background=random',
+            },
+            score: 0,
+          },
+          {
+            user: {
+              id: 'user-ganmara',
+              name: 'ganmara',
+              email: 'ganmara888@gmail.com',
+              avatar:
+                'https://ui-avatars.com/api/?name=ganmara&background=random',
+            },
+            score: 0,
+          },
+        ],
+      },
+    };
+
+    // Action
+    const nextState = leaderboardsReducer(initialState, action);
+
+    // Assert
+    expect(nextState).toEqual(action.payload.leaderboards);
   });
 });
