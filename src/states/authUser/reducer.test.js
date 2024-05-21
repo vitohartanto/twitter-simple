1246aba7e093b5c65144ccac3cecfbb7edcb030a
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-21 10:43:56 +0700

    authUserReducer automation test done

--- src/states/authUser/reducer.test.js
+++ src/states/authUser/reducer.test.js
@@ -1,10 +1,67 @@
+import { describe, it, expect } from 'vitest';
+import authUserReducer from './reducer';
+
+const AUTH_USER_ACTION_TYPES = {
+  SET_AUTH_USER: 'authUser/SET_AUTH_USER',
+  UNSET_AUTH_USER: 'authUser/UNSET_AUTH_USER',
+};
+
 /**
  * test scenario for talksReducer
  *
- * - talkReducers function
+ * - authUserReducer function
  *  - should return the initial state when given by unknown action
- *  - should return the talks when given by RECEIVE_TALKS action
- *  - should return the talks with the new talk when given by ADD_TALK action
- *  - should return the talks with the toggled like talk when given by TOGGLE_LIKE_TALK action
+ *  - should return the authUser when given by authUser/SET_AUTH_USER action
+ *  - should return the authUser with value null when given by authUser/UNSET_AUTH_USER action
  *
  */
+
+describe('authUserReducer function', () => {
+  it('should return the initial state when given by unknown action', () => {
+    // Arrange
+    const initialState = null;
+    const action = { type: 'UNKNOWN' };
+
+    // Action
+    const nextState = authUserReducer(initialState, action);
+
+    // Assert
+    expect(nextState).toEqual(initialState);
+  });
+
+  it('should return the authUser when given by authUser/SET_AUTH_USER action', () => {
+    // Arrange
+    const initialState = null;
+    const action = {
+      type: AUTH_USER_ACTION_TYPES.SET_AUTH_USER,
+      payload: {
+        authUser: {
+          id: 'user-ganmara',
+          name: 'ganmara',
+          email: 'ganmara888@gmail.com',
+          avatar: 'https://ui-avatars.com/api/?name=ganmara&background=random',
+        },
+      },
+    };
+
+    // Action
+    const nextState = authUserReducer(initialState, action);
+
+    // Assert
+    expect(nextState).toEqual(action.payload.authUser);
+  });
+
+  it('should return the authUser with value null when given by authUser/UNSET_AUTH_USER action', () => {
+    // Arrange
+    const initialState = null;
+    const action = {
+      type: AUTH_USER_ACTION_TYPES.UNSET_AUTH_USER,
+    };
+
+    // Action
+    const nextState = authUserReducer(initialState, action);
+
+    // Assert
+    expect(nextState).toEqual(null);
+  });
+});
