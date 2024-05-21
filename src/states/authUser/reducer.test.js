f408ddb77e330e062b4fc441cb0400f5747c799c
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-21 09:59:14 +0700

    testing reducer on state authUser

---
+++ src/states/authUser/reducer.test.js
@@ -0,0 +1,10 @@
+/**
+ * test scenario for talksReducer
+ *
+ * - talkReducers function
+ *  - should return the initial state when given by unknown action
+ *  - should return the talks when given by RECEIVE_TALKS action
+ *  - should return the talks with the new talk when given by ADD_TALK action
+ *  - should return the talks with the toggled like talk when given by TOGGLE_LIKE_TALK action
+ *
+ */
