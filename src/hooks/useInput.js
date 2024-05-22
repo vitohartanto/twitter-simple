56dbed842009e5fad9c1e292b3dffe5045b76359
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-22 22:56:13 +0700

    make ci test passed 2

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 
-// Custom Hook
 const useInput = () => {
   const [value, setValue] = useState('');
 
   const onChangeValueHandler = (event) => setValue(event.target.value);
 
-  return [value, onChangeValueHandler];
+  return [value, onChangeValueHandler, setValue];
 };
 
 export default useInput;
