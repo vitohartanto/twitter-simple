58c76754d8827489d7fe492275814dfb7bb64a15
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-22 22:03:21 +0700

    make test error2

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 const useInput = () => {
   const [value, setValue] = useState('');
 
+  const onChangeValueHandler = (event) => setValue(event.target.value);
+
   return [value];
 };
 
