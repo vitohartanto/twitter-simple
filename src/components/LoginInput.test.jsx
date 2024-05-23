1a3cd1e1a7454c7acdd79813abe9e16cb0a04883
Vito <vitohartanto@mail.ugm.ac.id>
2024-05-23 18:22:24 +0700

    adding component testing LoginInput

--- src/components/LoginInput.test.jsx
+++ src/components/LoginInput.test.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable comma-dangle */
 import React from 'react';
 // eslint-disable-next-line object-curly-newline
-import { describe, it, expect, afterEach } from 'vitest';
+import { describe, it, expect, afterEach, vi } from 'vitest';
 import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import matchers from '@testing-library/jest-dom/matchers';
@@ -53,5 +53,29 @@ describe('LoginInput component', () => {
 
     // Assert
     expect(emailInput).toHaveValue('ganmara');
+  });
+
+  it('should call login function when login button is clicked', async () => {
+    // Arrange
+    const mockLogin = vi.fn();
+    render(
+      <BrowserRouter>
+        <LoginInput login={mockLogin} />
+      </BrowserRouter>
+    );
+    const emailInput = screen.getByPlaceholderText('Email');
+    await userEvent.type(emailInput, 'ganmara888@gmail.com');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    await userEvent.type(passwordInput, 'ganmara');
+    const loginButton = screen.getByTitle('Login');
+
+    // Action
+    await userEvent.click(loginButton);
+
+    // Assert
+    expect(mockLogin).toHaveBeenCalledWith({
+      email: 'ganmara888@gmail.com',
+      password: 'ganmara',
+    });
   });
 });
